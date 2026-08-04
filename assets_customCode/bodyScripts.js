004d0a0c54363c1797b9ea7b68a1edce6ee0e730
Ethan Morrow <ethangmorrow@outlook.com>
2026-08-04 13:39:20 -0500

    website code updates, move all capacitor variables to bodyScripts.js

--- assets_customCode/bodyScripts.js
+++ assets_customCode/bodyScripts.js
@@ -1,5 +1,14 @@
+// Declare all Capacitor Integrations
 const FirebaseMessaging = window.Capacitor.Plugins.FirebaseMessaging;
+const FirebaseFunctions = window.Capacitor.Plugins.FirebaseFunctions;
 const Toast = window.Capacitor.Plugins.Toast;
+const Haptics = window.Capacitor.Plugins.Haptics;
+const App = window.Capacitor.Plugins.App;
+const BarcodeScanner = window.Capacitor.Plugins.CapacitorBarcodeScanner;
+const Browser = window.Capacitor.Plugins.Browser;
+const Dialog = window.Capacitor.Plugins.Dialog;
+const Geolocation = window.Capacitor.Plugins.Geolocation;
+
 // ----- Request notification permissions -----
 const handleRequestPermission = async () => {
   try {
--- assets_customCode/geolocationTest.js
+++ assets_customCode/geolocationTest.js
@@ -2,9 +2,6 @@ window.onload = async function () {
   const isApp = !!window.Capacitor;
 
   if (isApp) {
-    const Geolocation = window.Capacitor.Plugins.Geolocation;
-    const Toast = window.Capacitor.Plugins.Toast;
-
     const printCurrentPosition = async () => {
       const coordinates = await Geolocation.getCurrentPosition();
       console.log('Current position:', coordinates.coords);
